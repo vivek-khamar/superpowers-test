0ee49a95c3329b0a48a55a22c777f47d3a1022af
Khamar <vivek.khamar@smartsensesolutions.com>
2026-07-10 16:26:52 +0530

    fix(DEMO-1): remove unused FieldError import to satisfy Checkstyle

--- src/main/java/com/superpowers/test/auth/exception/GlobalExceptionHandler.java
+++ src/main/java/com/superpowers/test/auth/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.superpowers.test.auth.exception;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
